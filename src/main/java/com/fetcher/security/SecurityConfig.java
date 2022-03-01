7b0a3828d2889c2ca97f07989040e145293ef2ae
Camilo Hernandez <camehernandecas@unal.edu.co>
2022-03-01 11:58:03 -0500

    filter to generate token and refresh token

--- src/main/java/com/fetcher/security/SecurityConfig.java
+++ src/main/java/com/fetcher/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fetcher.security;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,6 +18,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.fetcher.security.filters.CustomAuthenticationFilter;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -25,6 +28,13 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private final UserDetailsService userDetailsService;
+	
+	@Value("${jwt.secret}")
+	private String JWT_SECRET;
+	@Value("${jwt.token.expiration_time}")
+	private long JWT_TOKEN_EXPIRATION_TIME;
+	@Value("${jwt.refresh_token.expiration_time}")
+	private long JWT_REFRESH_TOKEN_EXPIRATION_TIME;
 	
 	@Bean
 	PasswordEncoder passwordEncoder() {
@@ -35,33 +45,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
-//
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.cors().and().csrf().disable();
-//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//		http.authorizeRequests().anyRequest().permitAll();
-//		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-//	}
-//	
-//	@Bean
-//	@Override
-//	public AuthenticationManager authenticationManagerBean() throws Exception{
-//		return super.authenticationManagerBean();
-//	}
-//	
-//	@Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("*"));
-//        configuration.setAllowedHeaders(Arrays.asList("*"));
-//        configuration.setAllowCredentials(true);
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
-//	
-//	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean(), JWT_SECRET, JWT_TOKEN_EXPIRATION_TIME, JWT_REFRESH_TOKEN_EXPIRATION_TIME));
+	}
 	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception{
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
