97b68bfb85ffcde1f6fd8261f7e52944689ff826
Camilo Hernandez <camehernandecas@unal.edu.co>
2022-03-04 10:49:55 -0500

    database config

--- src/main/java/com/fetcher/security/SecurityApplication.java
+++ src/main/java/com/fetcher/security/SecurityApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class SecurityApplication {
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
 	}
